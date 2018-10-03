4f89f1628932da0d8286e0d97e35c5230de32f66
PauloCamargos <paulo.camargos@ufu.br>
2018-10-03 00:31:11 -0300

    nova equalizacao sem pontos pretos

--- equalizacao/equalizacao_sem_brancos.m
+++ equalizacao/equalizacao_sem_brancos.m
@@ -1,30 +1,52 @@
+% Lendo a imagem original
 im_in = imread('../imagens-base/Im_original.jpg');
+
+% Buscando os indices dos pixels que possuem valor zero
+indices_zeros = find(im_in < 7);
+
+% Equalizando a imagem
 imeq  = histeq(im_in);
+
+% Copia da imagem equalizada
+im_eq_modificada = imeq;
+
+% Reestabelecendo a cor 0 nos pixels que eram 0 originalmente
+im_eq_modificada(indices_zeros) = 0;
+
+% Lendo imagem com filtro do Instagram
 im_insta = imread('resultados/im_insta.jpg');
 
 % EQUALIZACAO
 figure
 % Imagem original
-subplot(2,3,1);
+subplot(2,4,1);
 imshow(im_in);
 title("Original image");
-subplot(2,3,4);
+subplot(2,4,5);
 imhist(im_in);
 title("Histogram");
 
 % Imagem equalizada
-subplot(2,3,2);
+subplot(2,4,2);
 imshow(imeq);
 title("Equalized image");
-subplot(2,3,5);
+subplot(2,4,6);
 imhist(imeq);
 title("Histogram");
 
+% Imagem equalizada com preto modificado
+subplot(2,4,3);
+imshow(im_eq_modificada);
+title("Equalized modified");
+subplot(2,4,7);
+imhist(im_eq_modificada);
+title("Histogram");
+
 % Filtro instagram
-subplot(2,3,3);
+subplot(2,4,4);
 imshow(im_insta);
 title("Instagram filter");
-subplot(2,3,6);
+subplot(2,4,8);
 imhist(im_insta);
 title("Histogram");
 
